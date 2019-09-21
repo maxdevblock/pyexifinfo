b3ca40f11a05b9a1d633694ead132045989f08c0
MaxDevBlock <46634650+maxdevblock@users.noreply.github.com>
2019-09-21 10:10:45 +0200

    Update pyexifinfo.py

--- pyexifinfo/pyexifinfo.py
+++ pyexifinfo/pyexifinfo.py
@@ -112,7 +112,7 @@ def get_json(filename):
 
     #Process this function
     filename = os.path.abspath(filename)
-    s = command_line(['exiftool', '-G', '-j', '-sort', filename])
+    s = command_line(['exiftool', '-c', '"%.6f"', '-G', '-j', '-sort', filename])
     if s:
         #convert bytes to string
         s = s.decode('utf-8').rstrip('\r\n')
